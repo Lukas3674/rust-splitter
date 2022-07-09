39c0dd46c4c60cac1d309ebfcea689f2b401b873
Lukas3674 <lukashassler.lh@gmail.com>
2022-07-09 23:11:38 +0200

    added custom info example in readme and docs

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,41 @@
 //! let re: Vec<&[usize]> = vec![&[1], &[2], &[3, 3], &[4]];
 //! assert_eq!(sp.collect::<Vec<_>>(), re);
 //! ```
+//! ## Custom Info
+//! ```rust
+//! use std::ops::Range;
+//! use splitter::{StrInfo, StrSplitter};
+//! 
+//! #[derive(Default)]
+//! struct CustomCtx {
+//!     cursor: usize,
+//! }
+//! 
+//! #[derive(Debug, PartialEq)]
+//! struct Custom<'a> {
+//!     content: &'a str,
+//!     span: Range<usize>,
+//! }
+//! 
+//! impl<'a> StrInfo<'a> for Custom<'a> {
+//!     type Context = CustomCtx;
+//!     fn generate(ctx: &mut Self::Context, ts: &'a str) -> Self {
+//!         let start = ctx.cursor;
+//!         ctx.cursor += ts.len();
+//!         Custom { content: ts, span: start..ctx.cursor }
+//!     }
+//! }
+//! 
+//! let sp = StrSplitter::new("bytes example", " ").with_info::<Custom>();
+//! assert_eq!(
+//!     sp.collect::<Vec<_>>(),
+//!     vec![
+//!         Custom { content: "bytes", span: 0..5 },
+//!         Custom { content: " ", span: 5..6 },
+//!         Custom { content: "example", span: 6..13 },
+//!     ],
+//! );
+//! ```
 //! 
 //! ## Derive Info
 //! When using the derive macro, and lifetimes are provided, the first lifetime
