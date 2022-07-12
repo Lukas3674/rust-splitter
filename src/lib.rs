55fb974bbb66f89ab71da816495db086c26f0b63
Lukas3674 <lukashassler.lh@gmail.com>
2022-07-12 18:17:24 +0200

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -45,6 +45,6 @@ pub use { string::info as str_info, string::separator as str_separator };
 #[cfg(feature = "derive")]
 pub use splitter_derive::*;
 
-pub use info::Info;
+pub use crate::info::Info;
 pub use crate::splitter::Splitter;
-pub use { string::StrInfo, string::StrSplitter };
+pub use { crate::string::StrInfo, crate::string::StrSplitter };
